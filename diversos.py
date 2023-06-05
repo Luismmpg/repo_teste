0308a66d54ddd3657b5b80d285b0fe4496fd0fdf
Luismmpg <luis.mmp.goncalves@gmail.com>
2023-06-05 19:17:01 +0100

    Adicionei umas linhas

--- diversos.py
+++ diversos.py
@@ -1,9 +1,8 @@
+# Comentei para fazer commit
 n = int(input("Comprimento do loop? "))
 numero = 1
 while numero <= n:
     print(numero)
     numero += 1
-    
-    
-    
 
+print("Fim do programa!")
